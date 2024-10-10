2e417b4b1bd0e20346f32a91fe1db96f80b74721
adrianvillagomez <adrian132892@gmail.com>
2024-10-10 00:30:49 +0000

    commit_final

--- RTA_SCRIPT_Examen_20241005/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_F.sh
@@ -6,4 +6,4 @@ echo "El Hash de mi Usuario es: $(sudo grep -i vagrant /etc/shadow | awk -F ':' 
 echo "La URL de mi reposiorio es : $(git remote get-url origin)"  
 } > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_VILLAGOMEZ/RTA_ARCHIVOS_Examen_20241009/Filtro_Avanzado.txt
 
-echo "Archivo creado"
+echo "Archivo creado."
